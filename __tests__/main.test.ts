72953abfe557d224324c0ae2d544ffeac3fb0e7d
Daryl Walleck <dwalleck@gmail.com>
2020-04-22 10:47:31 -0500

    Trivial commit to test a theory

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -3,7 +3,7 @@ import { run } from '../src/main'
 
 jest.mock('@actions/core')
 
-describe('When running the action', () => {
+describe('When running the GitHub Action', () => {
     const fakeSetOutput = core.setOutput as jest.MockedFunction<typeof core.setOutput>
 
     test('it should set the release-url output parameter', async () => {
