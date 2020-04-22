6c7cb25325616718e1436570e027424e3933e696
Daryl Walleck <dwalleck@gmail.com>
2020-04-22 11:18:51 -0500

    Trying a new idea

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import * as core from '@actions/core'
 
 export async function run(): Promise<void> {
   try {
+    core.debug('I am running the right function')
     core.setOutput('release-url', 'http://some-release.com')
   } catch (error) {
     core.setFailed(error.message)
